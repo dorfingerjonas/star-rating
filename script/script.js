62794f7964581a19a1323cef4e8bdfebc8f73597
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-19 10:41:30 +0200

    add boolean to each star if clicked

--- script/script.js
+++ script/script.js
@@ -6,7 +6,6 @@ function printStars() {
     const svg = '<svg class="star" id="placeholder" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="#353535" width="18px" height="18px"><g><rect fill="none" height="24" width="24"/><path d="M14.43,10l-1.47-4.84c-0.29-0.95-1.63-0.95-1.91,0L9.57,10H5.12c-0.97,0-1.37,1.25-0.58,1.81l3.64,2.6l-1.43,4.61 c-0.29,0.93,0.79,1.68,1.56,1.09L12,17.31l3.69,2.81c0.77,0.59,1.85-0.16,1.56-1.09l-1.43-4.61l3.64-2.6 c0.79-0.57,0.39-1.81-0.58-1.81H14.43z"/></g></svg>'
     const parent = document.getElementById('ratingWrapper');
     const starAmount = 5;
-    let ratingFixed = false;
 
     for (let i = 0; i < starAmount; i++) {
         parent.innerHTML += svg.replace('placeholder', `star${i + 1}`);
@@ -22,19 +21,23 @@ function printStars() {
         });
 
         star.addEventListener('mouseout', () => {
-            if (!ratingFixed) {
-                for (const str of parent.children) {
+            for (const str of parent.children) {
+                if (!str.isClicked) {
                     str.style.fill = '#353535';
                 }
             }
         });
 
         star.addEventListener('click', () => {
-            for (let i = id; i < parent.children.length; i++) {
-                parent.children[i].style.fill = '#353535';
+            for (let i = 0; i < id; i++) {
+                parent.children[i].isClicked = true;
             }
 
-            ratingFixed = true;
+            for (let i = id; i < parent.children.length; i++) {
+                parent.children[i].style.fill = '#353535';
+                parent.children[i].isClicked = false;
+            }
+
             console.log(`your rating: ${id}/${starAmount}`);
         });
     }
